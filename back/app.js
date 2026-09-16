42de9823718e856b0d45b1dd2b03550a4aeb1f50
a25ramcarmor <a25ramcarmor@inspedralbes.cat>
2026-09-16 10:11:32 +0200

    Servidor Funcionant amb el Helloworld i els 2 json

---
+++ back/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/json1',(req,res) =>{
+    const dades = fs.readFileSync('preguntes.json','utf-8');
+    res.send(dades);
+});
+app.get('/json2',(req,res) =>{
+    const dades2 = fs.readFileSync('respostes.json','utf-8');
+    res.send(dades2);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
